651dcd173b8256bdef1db74541bcdabd1bcf9565
kris <luckykris@yeah.net>
2023-06-19 16:07:57 +0800

    Delete script directory

--- script/dj_build.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p build/packages
-mkdir -p dist
-virtualenv -p /usr/local/bin/python3.6 ./build/venv
-source ./build/venv/bin/activate
-pip3 install -r requirements.txt
-cp -r handlers ./build/packages/
-cp -r connectors ./build/packages/
-cp -r webshell ./build/packages/
-cp -r templates ./build/packages/
